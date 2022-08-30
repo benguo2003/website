b19fa38f5515ae4f203306559127590c89c489bc
benguo2003 <benguo2003731@gmail.com>
2022-08-30 14:24:52 -0700

    funge

---
+++ index.js
@@ -0,0 +1,9 @@
+function showAccept() {
+    document.getElementById('deny').style.display = "none";
+    document.getElementById('accept').style.display = "block";
+}
+
+function showDeny() {
+    document.getElementById('accept').style.display = "none";
+    document.getElementById('deny').style.display = "block";
+}

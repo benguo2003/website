090fe4247d74b0f10ad374717193aef663efa3ca
benguo2003 <benguo2003731@gmail.com>
2022-09-01 17:33:02 -0700

    updates

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-function showAccept() {
-    document.getElementById('deny').style.display = "none";
-    document.getElementById('accept').style.display = "block";
+function changeMode() {
+    var element = document.body;
+    if( (' ' + element.className + ' ').indexOf(' light-mode ') > -1 ){
+        darkMode(element);
+    }
+    else{
+        lightMode(element);
+    }
 }
 
-function showDeny() {
-    document.getElementById('accept').style.display = "none";
-    document.getElementById('deny').style.display = "block";
+function darkMode(element) {
+    element.className = "";
+    document.getElementById('modeChange').src="/images/lightMode.webp";
 }
+
+function lightMode(element) {
+    element.className = "light-mode";
+    document.getElementById('modeChange').src="/images/darkMode.webp";
+}
+
+

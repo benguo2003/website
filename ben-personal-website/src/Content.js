202cf06b9b580990873625281d92827eadf8fe61
Ben Guo <benguo@Bens-MacBook-Pro.local>
2024-04-23 16:23:26 -0700

    updates

--- ben-personal-website/src/Content.js
+++ ben-personal-website/src/Content.js
@@ -1,54 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import github from './images/github.png';
 import linkedin from './images/linkedin.png';
 import pdf from './images/pdf.png';
 import { AwesomeButton } from "react-awesome-button";
 import 'react-awesome-button/dist/styles.css';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate} from 'react-router-dom';
 
-function NavigateButton() {
-    const navigate = useNavigate();
-
-    const navigateToBobaBolt = () => {
-        navigate('/bobabolt');
+function ModeButton({ isDarkMode, setIsDarkMode }) {
+    const toggleMode = () => {
+        setIsDarkMode(!isDarkMode);
+        document.documentElement.className = !isDarkMode ? 'dark-mode' : 'light-mode';
     };
 
     return (
         <div className="navigate-button">
-        <AwesomeButton type="secondary" onPress={navigateToBobaBolt}>
-            CLICK ME
-        </AwesomeButton>
+            <AwesomeButton type="secondary" onPress={toggleMode}>
+                {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+            </AwesomeButton>
         </div>
     );
 }
 
-function Content( isDarkMode ) {
+function Content() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+        <ModeButton isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
         <header>
-            <h1>Ben Guo</h1>
+            <h1>Ben Guo</h1>    
             <nav id="navigation"></nav>
         </header>
         <div class="content">
             <p> 
-                Hey there! I'm Ben, a junior majoring in Computer Science <br/>
+                Hey there! I'm Ben, a junior majoring in Computer Science
                 at the University of California, Los Angeles.
             </p> 
-            <p>
-                Currently, I'm working @ AvanSight as a software engineer intern, <br/>
-                where I'm developing an a sophisticated web application tailored to <br/>
-                efficiently receive, process, and organize data obtained from <br/>
-                pharmaceutical companies' studies, culminating in a user-friendly <br/>
-                online interface for seamless navigation.
+            <p> 
+                I'm looking forward to becoming a software engineer
+                or a game developer after graduation.
             </p> 
             <p> 
-                I'm looking forward to becoming a software engineer, <br/>
-                and more specifically, a game developer after graduation.
-            </p> 
-            <p> 
-                You can find me at any of these platforms below, <br/> 
-                or check out my resume by clicking on the last icon. <br/>
+                You can find me at any of these platforms below,
+                or check out my resume by clicking on the last icon.
                 Also, feel free to reach out at junbinbenguo@gmail.com!
             </p>  
         </div>
@@ -63,7 +57,6 @@ function Content( isDarkMode ) {
                 <img id="pdf" src={pdf} alt="pdf"/>
             </a>
         </div>
-        <NavigateButton />
     </div>
   );
 }

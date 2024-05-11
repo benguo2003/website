85d76fd1ed04733af569258f6a710b05b80d0781
Ben Guo <benguo@Bens-MacBook-Pro.local>
2024-05-11 04:33:53 -0700

    surprise

--- ben-personal-website/src/Content.js
+++ ben-personal-website/src/Content.js
@@ -5,6 +5,9 @@ import linkedin from './images/linkedin.png';
 import pdf from './images/pdf.png';
 import { AwesomeButton } from "react-awesome-button";
 import 'react-awesome-button/dist/styles.css';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { LocalFlorist } from '@material-ui/icons';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 
 function ModeButton({ isDarkMode, setIsDarkMode }) {
     const toggleMode = () => {
@@ -23,42 +26,61 @@ function ModeButton({ isDarkMode, setIsDarkMode }) {
 
 function Content() {
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isIconClicked, setIconClicked] = useState(false);
 
-  return (
-    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
-        <ModeButton isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
-        <header>
-            <h1>Ben Guo</h1>    
-            <nav id="navigation"></nav>
-        </header>
-        <div class="content">
-            <p> 
-                Hey there! I'm Ben, a junior majoring in Computer Science
-                at the University of California, Los Angeles.
-            </p> 
-            <p> 
-                I'm looking forward to becoming a software engineer
-                or a game developer after graduation.
-            </p> 
-            <p> 
-                You can find me at any of these platforms below,
-                or check out my resume by clicking on the last icon.
-                Also, feel free to reach out at junbinbenguo@gmail.com!
-            </p>  
+    const handleIconClick = () => {
+        setIconClicked(!isIconClicked);
+    };
+
+    return (
+        <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+            <SvgIcon style={{ position: 'fixed', top: 0, left: 0, margin: 10, cursor: 'pointer' }} onClick={handleIconClick}>
+                <LocalFlorist />
+            </SvgIcon>
+            <ModeButton isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+            {isIconClicked ? (
+                <div class="content">
+                    <h1>I'm sorry bobabolt i lava you very very much</h1>
+                    <SvgIcon style={{fontSize: 100}}>
+                        <VolunteerActivismIcon />
+                    </SvgIcon>
+                </div>
+            ) : (
+                <>
+                    <header>
+                        <h1>Ben Guo</h1>    
+                        <nav id="navigation"></nav>
+                    </header>
+                    <div class="content">
+                        <p> 
+                            Hey there! I'm Ben, a junior majoring in Computer Science
+                            at the University of California, Los Angeles.
+                        </p> 
+                        <p> 
+                            I'm looking forward to becoming a software engineer
+                            or a game developer after graduation.
+                        </p> 
+                        <p> 
+                            You can find me at any of these platforms below,
+                            or check out my resume by clicking on the last icon.
+                            Also, feel free to reach out at junbinbenguo@gmail.com!
+                        </p>  
+                    </div>
+                    <div className="iconBar">
+                        <a href="https://www.github.com/benguo2003" target="_blank" rel="noopener noreferrer">
+                            <img id="github" src={github} alt="github"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/benguo2003/" target="_blank" rel="noopener noreferrer">
+                            <img id="linkedin" src={linkedin} alt="linkedin"/>
+                        </a>
+                        <a href={`${process.env.PUBLIC_URL}/Ben_Resume.pdf`} target="_blank" rel="noopener noreferrer">
+                            <img id="pdf" src={pdf} alt="pdf"/>
+                        </a>
+                    </div>
+                </>
+            )}
         </div>
-        <div className="iconBar">
-            <a href="https://www.github.com/benguo2003" target="_blank" rel="noopener noreferrer">
-                <img id="github" src={github} alt="github"/>
-            </a>
-            <a href="https://www.linkedin.com/in/benguo2003/" target="_blank" rel="noopener noreferrer">
-                <img id="linkedin" src={linkedin} alt="linkedin"/>
-            </a>
-            <a href={`${process.env.PUBLIC_URL}/Ben_Resume.pdf`} target="_blank" rel="noopener noreferrer">
-                <img id="pdf" src={pdf} alt="pdf"/>
-            </a>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Content;

b6f54ee705e7a09708dce357b743e5766b3c8f42
benguo <bguo@fulgentgenetics.com>
2025-01-22 10:18:44 -0800

    res update

--- ben-personal-website/src/Content.js
+++ ben-personal-website/src/Content.js
@@ -34,7 +34,7 @@ function Content() {
     const [selection, setSelection] = useState(null);
 
     const handleIconClick = () => {
-        setIconClicked(!isIconClicked);
+        //setIconClicked(!isIconClicked);
     };
 
     return (
@@ -59,12 +59,7 @@ function Content() {
                     </header>
                     <div class="content">
                         <p> 
-                            Hey there! I'm Ben, a Senior majoring in Computer Science
-                            at the University of California, Los Angeles.
-                        </p> 
-                        <p> 
-                            I'm currently interning at Fulgent Genetics and looking forward to becoming 
-                            a software engineer or a software developer after graduation.
+                            Hey there! I'm Ben, currently working as a Software Engineer at Fulgent Genetics.
                         </p> 
                         <p> 
                             You can find me at any of these platforms below,
@@ -79,7 +74,7 @@ function Content() {
                         <a href="https://www.linkedin.com/in/benguo2003/" target="_blank" rel="noopener noreferrer">
                             <img id="linkedin" src={linkedin} alt="linkedin"/>
                         </a>
-                        <a href={`${process.env.PUBLIC_URL}/Ben_Resume.pdf`} target="_blank" rel="noopener noreferrer">
+                        <a href={`${process.env.PUBLIC_URL}/Updated_Resume.pdf`} target="_blank" rel="noopener noreferrer">
                             <img id="pdf" src={pdf} alt="pdf"/>
                         </a>
                     </div>
